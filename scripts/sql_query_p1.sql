648cf8a11bffea6c808723b607ef14981d3a4be1
Akhilendra pratap <147452279+Akhilp12346@users.noreply.github.com>
2026-03-14 16:08:06 +0530

    Update sql_query_p1.sql

--- scripts/sql_query_p1.sql
+++ scripts/sql_query_p1.sql
@@ -162,33 +162,36 @@ ORDER BY category
 
 -- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
 
-SELECT 
-       year,
-       month,
-    avg_sale
-FROM 
-(    
-SELECT 
-    EXTRACT(YEAR FROM sale_date) as year,
-    EXTRACT(MONTH FROM sale_date) as month,
-    AVG(total_sale) as avg_sale,
-    RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) as rank
-FROM retail_sales
-GROUP BY 1, 2
-) as t1
-WHERE rank = 1
+SELECT
+    year,
+    month,
+    avg_Sale
+FROM
+(
+    SELECT
+        YEAR(sale_date) AS year,
+        MONTH(sale_date) AS month,
+        AVG(total_sale) AS avg_Sale,
+        RANK() OVER (PARTITION BY YEAR(sale_date) ORDER BY AVG(total_sale) DESC) AS rank
+    FROM
+        retail_sale
+    GROUP BY
+        YEAR(sale_date),
+        MONTH(sale_date)
+) AS t
+WHERE
+    rank = 1;
     
--- ORDER BY 1, 3 DESC
 
 -- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
 
-SELECT 
+SELECT TOP 5
     customer_id,
-    SUM(total_sale) as total_sales
+    SUM(total_sale) as total_sale
 FROM retail_sales
-GROUP BY 1
-ORDER BY 2 DESC
-LIMIT 5
+GROUP BY customer_id
+ORDER BY total_sale DESC
+
 
 -- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
 
@@ -203,22 +206,27 @@ GROUP BY category
 
 -- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17)
 
-WITH hourly_sale
-AS
+WITH hourly_sale AS
 (
-SELECT *,
-    CASE
-        WHEN EXTRACT(HOUR FROM sale_time) < 12 THEN 'Morning'
-        WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
-        ELSE 'Evening'
-    END as shift
-FROM retail_sales
+    SELECT DATEPART(HOUR, sale_time) AS sale_hour
+    FROM retail_sale
+),
+shift_data AS
+(
+    SELECT
+        CASE 
+            WHEN sale_hour < 12 THEN 'Morning'
+            WHEN sale_hour BETWEEN 12 AND 17 THEN 'Afternoon'
+            ELSE 'Evening'
+        END AS shift_time
+    FROM hourly_sale
 )
-SELECT 
-    shift,
-    COUNT(*) as total_orders    
-FROM hourly_sale
-GROUP BY shift
+
+SELECT
+    shift_time,
+    COUNT(*) AS total_orders
+FROM shift_data
+GROUP BY shift_time;
 
 -- End of project
 
